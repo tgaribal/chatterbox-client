1425b8de54c72f07c8f9ea871512cf56059d11e2
tgaribal <tgaribal@gmail.com>
2016-09-26 17:29:20 -0700

    Add ability to create new messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,50 +1,111 @@
 // YOUR CODE HERE:
 
 
-var ChatterBox = function () {
+class ChatterBox {
+  constructor() {
+    this.currentRoom = 'lobby';
+    this.allRooms = new Set();
+    this.addedRooms = new Set();
+    this.addedMessages = {};
 
-};
-
-
-ChatterBox.prototype.init = function () {
-
-};
-
-ChatterBox.prototype.displayRoomMessages = function(roomname) {
-  var ourContext = this;
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      for (var i = 0; i < data.results.length; i++) {
-        ourContext.displayMessage(data.results[i]);
+  }
+  displayRoomMessages () {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages?order=-createdAt',
+      type: 'GET',
+      contentType: 'application/json',
+      success: (data) => {
+        for (var i = 0; i < data.results.length; i++) {
+          var ourResult = data.results[i];
+          this.allRooms.add(ourResult.roomname);
+          var id = ourResult.objectId;
+          if ( !(id in this.addedMessages)) {
+            this.addedMessages[id] = ourResult;
+            if (ourResult.roomname === this.currentRoom) {
+              this.displayMessage(ourResult);
+            }
+            this.addedMessages[id] = ourResult;
+          }
+        }
+        this.updateRooms();
+      },
+      error: (data) => {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
       }
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to receive message', data);
+
+    });
+  }
+  init () {
+
+    this.displayRoomMessages();
+
+    $('.submit-button').click((e) => {
+      this.createMessage();
+    });
+
+    setInterval( () => {
+      this.displayRoomMessages();
+    }, 1000);
+  }
+
+    
+  updateRooms () {
+    for (var room of this.allRooms) {
+      if ( !this.addedRooms.has(room) ) {
+        var optionString = `<option value="${room}">${room}</option>`;
+        $('.room-choice').append(optionString);
+        this.addedRooms.add(room);
+      }
     }
+  }
+  displayMessage (messageObj) {
+    var div1 = wrapDiv(messageObj.text);
+    var div2 = wrapDiv(messageObj.username + ':', 'username');
+    var div3 = wrapDiv(messageObj.createdAt + ':', 'createdAt');
 
-  });
-};
+    var bigDiv = wrapDiv(div2 + div1 + div3, 'chat');
 
-ChatterBox.prototype.displayMessage = function (messageObj) {
-  var div1 = wrapDiv(messageObj.text);
-  var div2 = wrapDiv(messageObj.username + ':', 'username');
+    $('#chats').prepend(bigDiv);
+  }
+  createMessage () {
+    var message = $('.create-message').val();
+    var messageObj = this.generateMessage(message);
+    var messageJSON = JSON.stringify(messageObj);
+    console.log(messageObj);
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'POST',
+      data: messageJSON,
+      contentType: 'application/json',
+      success: (data) => {
+        debugger;
+        console.log('chatterbox: Message sent');
+      },
+      error: (data) => {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+  generateMessage (message) {
+    var messageObj = {};
+    messageObj.text = message;
+    messageObj.roomname = this.currentRoom;
+    messageObj.username = this.getUserName();
+    //messageObj.createdAt = Date.now();
+    return messageObj;
+  }
 
-  var bigDiv = wrapDiv(div2 + div1, 'chat');
+  getUserName () {
+    var userName = window.location.search.slice(10);
+    return userName;
+  }
+}
 
-  $('#chats').append(bigDiv);
-};
-
-var app = new ChatterBox();
-
-app.displayRoomMessages('lobby');
-
-
-var wrapDiv = function (text, className) {
+var wrapDiv = (text, className) => {
   var output = '<div';
   if (className) {
     output += ' class=' + className;
@@ -54,7 +115,10 @@ var wrapDiv = function (text, className) {
   return output;
 };
 
-
+var app = new ChatterBox();
+$(document).ready( () => {
+  app.init();
+});
 /*
 
 $.ajax({
